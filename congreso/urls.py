fb7a74bd95c403c779564c0031e323bcff62c347
JhonHuanacuni <jhon.huanacuni.mamani@gmail.com>
2024-05-04 11:26:47 -0500

    agregando la documentacion

--- congreso/urls.py
+++ congreso/urls.py
@@ -1,5 +1,6 @@
 #rutas que necesita el FronEnt
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers #agarra la vista y geenra todo los Urls
 from congreso import views
 
@@ -8,7 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'congreso')
 
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="Paso A"))
 ]
 
 
