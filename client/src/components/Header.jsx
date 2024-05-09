13ef16ce9f281486d24adab37e7a238f299776a3
JhonHuanacuni <jhon.huanacuni.mamani@gmail.com>
2024-05-09 13:19:00 -0500

    avanzando con el header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import  logo  from "../assets/img/logo_1.jpg";
+import  logo  from "../assets/img/logoEgatur.svg";
+import { NavBar } from "./NavBar";
 
 export function Header() {
   return (
-    <header className=" bg-red-400">
-        <img src={logo} alt="Logo" />
+    <header className=" bg-red-400 flex place-content-between items-center">
+        <img src={logo} alt="Logo" className="max-w-40  h-auto" />
+        <NavBar/>
     </header>
   )
 }
